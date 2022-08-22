6c55abc48d0f59ad304cb7fa855d775aa06bffe0
N0rdeanum <maksef87@gmail.com>
2022-08-22 17:25:18 +0300

    Добавил буферные вкладки

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -10,7 +10,6 @@ packer.startup(function(use)
   use "wbthomason/packer.nvim"
 
   -- Темы
-  use "lunarvim/synthwave84.nvim"
   use {
     "svrana/neosolarized.nvim",
     requires = { "tjdevries/colorbuddy.nvim" }
