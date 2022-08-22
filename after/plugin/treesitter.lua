c85bfb279d7cd2f00b9102b4326be5605d3651b3
N0rdeanum <maksef87@gmail.com>
2022-08-22 09:38:25 +0300

    Добавил конфиг treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,8 +1,31 @@
-local status, ts = pcall(require, "nvim-treesitter.config")
+local status, M = pcall(require, "nvim-treesitter.configs")
 if (not status) then
   return
 end
 
-ts.setup {
-
+M.setup {
+  highlight = {
+    enable = true,
+    disable = {},
+  },
+  indent = {
+    enable = true,
+    disable = {},
+  },
+  ensure_installed = {
+    "cpp",
+    "html",
+    "typescript",
+    "markdown",
+    "make",
+    "c",
+    "python",
+    "tsx",
+    "lua",
+    "json",
+    "css"
+  },
+  autotag = {
+    enable = true,
+  }
 }
