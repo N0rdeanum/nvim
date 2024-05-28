31f9e16753ef3439befc97386f79138f2f64d534
grassang <grassang@student.21-school.ru>
2024-05-28 18:15:53 +0300

    fix themes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -13,66 +13,66 @@
 --
 --
 --
--- return {
---   {
---     "diegoulloao/neofusion.nvim",
---     priority = 1000,
---     config = function()
---       require("neofusion").setup({
---         terminal_colors = true, -- add neovim terminal colors
---         undercurl = true,
---         underline = true,
---         bold = true,
---         italic = {
---           strings = true,
---           emphasis = true,
---           comments = true,
---           operators = false,
---           folds = true,
---         },
---         strikethrough = true,
---         invert_selection = false,
---         invert_signs = false,
---         invert_tabline = false,
---         invert_intend_guides = false,
---         inverse = true, -- invert background for search, diffs, statuslines and errors
---         palette_overrides = {},
---         overrides = {
---           Normal = { bg = "NONE" },
---           NonText = { bg = "NONE" },
---           LineNr = { bg = "NONE" },
---           Folded = { bg = "NONE" },
---           EndOfBuffer = { bg = "NONE" },
---         },
---         dim_inactive = false,
---         transparent_mode = true,
---       })
---       vim.cmd([[ colorscheme neofusion ]])
---     end,
---   },
--- }
---
---
 return {
-  "lvim-tech/lvim-colorscheme",
-  config = function()
-    require("lvim-colorscheme").setup({
-      style = "darks", -- или "darksoft", "light"
-      -- transparent_mode = true,
-      styles = {
-        comments = { italic = true, bold = true },
-        keywords = { italic = true, bold = true },
-        functions = { italic = true, bold = true },
-        variables = {},
-      },
-      sidebars = {},
-      colors = {
-        dark = {},
-        darksoft = {},
-        light = {},
-      },
-    })
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-  end,
+  {
+    "diegoulloao/neofusion.nvim",
+    priority = 1000,
+    config = function()
+      require("neofusion").setup({
+        terminal_colors = true, -- add neovim terminal colors
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+          strings = true,
+          emphasis = true,
+          comments = true,
+          operators = false,
+          folds = true,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = true, -- invert background for search, diffs, statuslines and errors
+        palette_overrides = {},
+        overrides = {
+          Normal = { bg = "NONE" },
+          NonText = { bg = "NONE" },
+          LineNr = { bg = "NONE" },
+          Folded = { bg = "NONE" },
+          EndOfBuffer = { bg = "NONE" },
+        },
+        dim_inactive = false,
+        transparent_mode = true,
+      })
+      vim.cmd([[ colorscheme neofusion ]])
+    end,
+  },
 }
+
+--
+-- return {
+--   "lvim-tech/lvim-colorscheme",
+--   config = function()
+--     require("lvim-colorscheme").setup({
+--       style = "darks", -- или "darksoft", "light"
+--       -- transparent_mode = true,
+--       styles = {
+--         comments = { italic = true, bold = true },
+--         keywords = { italic = true, bold = true },
+--         functions = { italic = true, bold = true },
+--         variables = {},
+--       },
+--       sidebars = {},
+--       colors = {
+--         dark = {},
+--         darksoft = {},
+--         light = {},
+--       },
+--     })
+--     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--   end,
+-- }
